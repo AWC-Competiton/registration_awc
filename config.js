cf043ac2185a110f6c646757fba2ed51505bd60b
awc_competition <mntbkyt@gmail.com>
2026-08-13 18:07:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,12 @@ const SUPABASE_ANON_KEY = "sb_publishable__95y3un-2F_YjD_yGyG1qw_tenZJtmY";
 
 // URL រូប KHQR ដែលដាក់ក្នុង GitHub ឬ Supabase Storage
 const REGISTRATION_FEE_TEXT = "ថ្លៃចុះឈ្មោះ: $1.0 សម្រាប់ជុំទីមួយ";
+
+const TELEGRAM_LINKS = {
+    "៧": "https://t.me/GRADE_7",
+    "៨": "https://t.me/GRADE_8",
+    "៩": "https://t.me/GRADE_9",
+    "១០": "https://t.me/GRADE_10",
+    "១១": "https://t.me/GRADE_11",
+    "១២": "https://t.me/GRADE_12"
+};
