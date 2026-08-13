b03c346f5b74b7cc0af6c1e08fd32d768adeb716
awc_competition <mntbkyt@gmail.com>
2026-08-13 18:33:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,20 @@
-// ប្តូរតម្លៃទាំងនេះទៅជា Supabase Project របស់អ្នក
+// config.js
+
 const SUPABASE_URL = "https://nahoygudhnbwhilcioma.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable__95y3un-2F_YjD_yGyG1qw_tenZJtmY";
 
-// URL រូប KHQR ដែលដាក់ក្នុង GitHub ឬ Supabase Storage
 const REGISTRATION_FEE_TEXT = "ថ្លៃចុះឈ្មោះ: $1.0 សម្រាប់ជុំទីមួយ";
 
 const TELEGRAM_LINKS = {
+    // លេខអារ៉ាប់
+    "7": "https://t.me/+PkpWrIgzBX0yNTBl",
+    "8": "https://t.me/+3GDpbG_a2kE4ZGU1",
+    "9": "https://t.me/+8BvP5sWptFlhYWFl",
+    "10": "https://t.me/+zp8gkhx3vyU1ZjFl",
+    "11": "https://t.me/+4dRf5Hjb-zM0NGI1",
+    "12": "https://t.me/+xVyL6dsR4ooxNTU9",
+
+    // លេខខ្មែរ
     "៧": "https://t.me/+PkpWrIgzBX0yNTBl",
     "៨": "https://t.me/+3GDpbG_a2kE4ZGU1",
     "៩": "https://t.me/+8BvP5sWptFlhYWFl",
